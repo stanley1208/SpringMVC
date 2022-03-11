c9efc2273de366f2da829211e293be3d00724665
stanley1208 <stanley.mei08@gmail.com>
2022-03-11 20:46:55 +0800

    java

---
+++ src/main/java/com/study/springmvcstudy/lab/entity/Fund.java
@@ -0,0 +1,43 @@
+package com.study.springmvcstudy.lab.entity;
+
+import java.util.Date;
+import java.util.List;
+
+public class Fund {
+	private Integer fid;
+	private String fname;
+	private Date createtime;
+	private List<Fundstock>fundstocks;
+	
+	public Integer getFid() {
+		return fid;
+	}
+	public void setFid(Integer fid) {
+		this.fid = fid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public Date getCreatetime() {
+		return createtime;
+	}
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+	public List<Fundstock> getFundstocks() {
+		return fundstocks;
+	}
+	public void setFundstocks(List<Fundstock> fundstocks) {
+		this.fundstocks = fundstocks;
+	}
+	@Override
+	public String toString() {
+		return "Fund [fid=" + fid + ", fname=" + fname + ", createtime=" + createtime + ", fundstocks=" + fundstocks
+				+ "]";
+	}
+	
+	
+}
