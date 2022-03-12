5419eac6f2b1b6c7657c473b14621b198538470e
stanley1208 <stanley.mei08@gmail.com>
2022-03-12 17:56:40 +0800

    java

--- src/main/java/com/study/springmvcstudy/lab/repository/FundDaoImpl.java
+++ src/main/java/com/study/springmvcstudy/lab/repository/FundDaoImpl.java
@@ -1,24 +1,53 @@
 package com.study.springmvcstudy.lab.repository;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.study.springmvcstudy.lab.entity.Fund;
+import com.study.springmvcstudy.lab.entity.Fundstock;
 
 @Repository
-public class FundDaoImpl implements FundDao{
+public class FundDaoImpl implements FundDao {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Override
 	public List<Fund> queryAll() {
-		String sql="select f.fid,f.fname,f.createtime from fund f order by f.fid";
+		return queryAllCase2();
+	}
+
+	private List<Fund> queryAllCase1() {
+		String sql = "select f.fid,f.fname,f.createtime from fund f order by f.fid";
 		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Fund>(Fund.class));
+	}
+
+	private List<Fund> queryAllCase2() {
+		String sql = "select f.fid,f.fname,f.createtime from fund f order by f.fid";
+		RowMapper<Fund> rm = (ResultSet rs, int rowNum) -> {
+			Fund fund = new Fund();
+			fund.setFid(rs.getInt("fid"));
+			fund.setFname(rs.getString("fname"));
+			fund.setCreatetime(rs.getDate("createtime"));
+			// 根據 fid 查詢 fundstock 列表
+			String sql2 = "select s.sid,s.fid,s.symbol,s.share " 
+							+ "from fundstock s " 
+							+ "where s.sid = ? "
+							+ "order by s.sid";
+			Object[] args = { fund.getFid() };
+			List<Fundstock> fundstocks = jdbcTemplate.query(sql2, args,
+					new BeanPropertyRowMapper<Fundstock>(Fundstock.class));
+			fund.setFundstocks(fundstocks);
+			return fund;
+		};
+
+		return jdbcTemplate.query(sql, rm);
 	}
 
 	@Override
