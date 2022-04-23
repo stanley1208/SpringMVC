0d956561319a0734ead32901a10b63daa9b314b0
A8250092 <A8250092@g.pccu.eddu.tw>
2022-04-23 16:12:06 +0800

    update

--- src/main/java/com/study/springmvcstudy/lab/controller/FundController.java
+++ src/main/java/com/study/springmvcstudy/lab/controller/FundController.java
@@ -4,23 +4,67 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.study.springmvcstudy.lab.entity.Fund;
 import com.study.springmvcstudy.lab.repository.FundDao;
 
-@Controller
+@RestController
 @RequestMapping("/lab/fund")
 public class FundController {
-	
+	private int pageNumber;
 	@Autowired
 	private FundDao fundDao;
 	
 	@GetMapping("/")
-	@ResponseBody
-	public List<Fund> index(){
-		return fundDao.queryAll();
+	public List<Fund> index() {
+		List<Fund> funds = fundDao.queryAll();
+		// 將沒有成分股的基金其 fundstocks 屬性置空(null)
+		funds.stream()
+			.filter(f -> f.getFundstocks() != null)
+			.filter(f -> f.getFundstocks().get(0).getFid() == null)
+			.forEach(f -> f.setFundstocks(null));
+		return funds;
 	}
+	
+	@GetMapping("/{fid}")
+	public Fund get(@PathVariable("fid") Integer fid) {
+		return fundDao.get(fid);
+	}
+	
+	@GetMapping("/totalPagecount")
+	public int totalPagecount() {
+		return (int)Math.ceil(fundDao.count() / (double)FundDao.LIMIT);
+	}
+	
+	@GetMapping("/page/{pageNumber}")
+	public List<Fund> page(@PathVariable("pageNumber") int pageNumber) {
+		this.pageNumber = pageNumber;
+		int offset = (pageNumber-1) * FundDao.LIMIT;
+		return fundDao.queryPage(offset);
+	}
+	
+	@PostMapping("/")
+	public int add(@RequestBody Fund fund) {
+		return fundDao.add(fund);
+	}
+	
+	@PutMapping("/")
+	public int update(@RequestBody Fund fund) {
+		return fundDao.update(fund);
+	}
+	
+	@DeleteMapping("/{fid}")
+	public int delete(@PathVariable("fid") Integer fid) {
+		return fundDao.delete(fid);
+	}
+	
 }
--- src/main/java/com/study/springmvcstudy/lab/repository/FundDaoImpl.java
+++ src/main/java/com/study/springmvcstudy/lab/repository/FundDaoImpl.java
@@ -17,93 +17,122 @@ import com.study.springmvcstudy.lab.entity.Fundstock;
 
 @Repository
 public class FundDaoImpl implements FundDao {
-
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-
+	
 	@Override
 	public List<Fund> queryAll() {
 		return queryAllCase3();
 	}
-
+	
 	private List<Fund> queryAllCase1() {
-		String sql = "select f.fid,f.fname,f.createtime from fund f order by f.fid";
+		String sql = "select f.fid, f.fname, f.createtime from fund f order by f.fid";
 		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Fund>(Fund.class));
 	}
-
+	
 	private List<Fund> queryAllCase2() {
-		String sql = "select f.fid,f.fname,f.createtime from fund f order by f.fid";
+		String sql = "select f.fid, f.fname, f.createtime from fund f order by f.fid";
 		RowMapper<Fund> rm = (ResultSet rs, int rowNum) -> {
 			Fund fund = new Fund();
 			fund.setFid(rs.getInt("fid"));
 			fund.setFname(rs.getString("fname"));
 			fund.setCreatetime(rs.getDate("createtime"));
 			// 根據 fid 查詢 fundstock 列表
-			String sql2 = "select s.sid,s.fid,s.symbol,s.share " 
-							+ "from fundstock s " 
-							+ "where s.fid = ? "
-							+ "order by s.sid";
-			Object[] args = { fund.getFid() };
-			List<Fundstock> fundstocks = jdbcTemplate.query(sql2, args,
+			String sql2 = "select s.sid, s.fid, s.symbol, s.share "
+					+ "from fundstock s "
+					+ "where s.fid = ? "
+					+ "order by s.sid";
+			Object[] args = {fund.getFid()};
+			List<Fundstock> fundstocks = jdbcTemplate.query(
+					sql2, 
+					args, 
 					new BeanPropertyRowMapper<Fundstock>(Fundstock.class));
 			fund.setFundstocks(fundstocks);
 			return fund;
 		};
-
 		return jdbcTemplate.query(sql, rm);
 	}
 	
 	// SimpleFlatMapping
-	// 調用 org.simpleflatmapper.jdbc.spring.jdbcTemplateMapperFactory
-	private List<Fund>queryAllCase3(){
+	// 調用 org.simpleflatmapper.jdbc.spring.JdbcTemplateMapperFactory
+	private List<Fund> queryAllCase3() {
 		// fundstocks_sid 其中 fundstocks 指的是 Fund.java 一對多的屬性命名
-		String sql="select f.fid , f.fname , f.createtime , "
-				+ "s.sid as fundstocks_sid, s.fid as fundstocks_fid, s.symbol as fundstocks_symbol, s.share as fundstocks_share "
+		String sql = "select f.fid , f.fname , f.createtime , "
+				+ "s.sid as fundstocks_sid , s.fid as fundstocks_fid , s.symbol as fundstocks_symbol , s.share as fundstocks_share  "
 				+ "from fund f left join fundstock s "
 				+ "on f.fid = s.fid";
-		ResultSetExtractor<List<Fund>>resultSetExtractor=
-				JdbcTemplateMapperFactory
-				.newInstance()
+		ResultSetExtractor<List<Fund>> resultSetExtractor = 
+				JdbcTemplateMapperFactory.newInstance()
 				.addKeys("fid") // Fund 的主鍵
 				.newResultSetExtractor(Fund.class);
-				
+		
 		return jdbcTemplate.query(sql, resultSetExtractor);
 	}
+	
 
 	@Override
 	public List<Fund> queryPage(int offset) {
-		// TODO Auto-generated method stub
-		return null;
+		if(offset < 0) {
+			return queryAll();
+		}
+		String sql = "select f.fid, f.fname, f.createtime from fund f order by f.fid ";
+		sql += String.format(" limit %d offset %d ", FundDao.LIMIT, offset);
+		RowMapper<Fund> rm = (ResultSet rs, int rowNum) -> {
+			Fund fund = new Fund();
+			fund.setFid(rs.getInt("fid"));
+			fund.setFname(rs.getString("fname"));
+			fund.setCreatetime(rs.getDate("createtime"));
+			// 根據 fid 查詢 fundstock 列表
+			String sql2 = "select s.sid, s.fid, s.symbol, s.share "
+					+ "from fundstock s "
+					+ "where s.fid = ? "
+					+ "order by s.sid";
+			Object[] args = {fund.getFid()};
+			List<Fundstock> fundstocks = jdbcTemplate.query(
+					sql2, 
+					args, 
+					new BeanPropertyRowMapper<Fundstock>(Fundstock.class));
+			fund.setFundstocks(fundstocks);
+			return fund;
+		};
+		return jdbcTemplate.query(sql, rm);
 	}
 
 	@Override
-	public Fund get(Integer sid) {
-		// TODO Auto-generated method stub
-		return null;
+	public Fund get(Integer fid) {
+		String sql = "select f.fid, f.fname, f.createtime from fund f where f.fid=?";
+		Fund fund = jdbcTemplate.queryForObject(sql, new Object[] {fid}, new BeanPropertyRowMapper<Fund>(Fund.class));
+		sql = "select s.sid, s.fid, s.symbol, s.share from fundstock s where s.fid = ?";
+		List<Fundstock> fundstocks = jdbcTemplate.query(
+				sql, 
+				new Object[] {fund.getFid()}, 
+				new BeanPropertyRowMapper<Fundstock>(Fundstock.class));
+		fund.setFundstocks(fundstocks);
+		return fund;
 	}
-
+	
 	@Override
 	public int count() {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "select count(*) from fund";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
 	}
 
 	@Override
 	public int add(Fund fund) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "insert into fund(fname) values (?)";
+		return jdbcTemplate.update(sql, fund.getFname());
 	}
 
 	@Override
 	public int update(Fund fund) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "update fund set fname=? where fid=?";
+		return jdbcTemplate.update(sql, fund.getFname(), fund.getFid());
 	}
 
 	@Override
 	public int delete(Integer fid) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "delete from fund where fid=?";
+		return jdbcTemplate.update(sql, fid);
 	}
 
 }
